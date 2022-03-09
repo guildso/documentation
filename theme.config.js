42cd80de4fb3316f03aabbcadec87d2da5b17ec6
Tony Lea <tnylea@gmail.com>
2022-03-09 14:53:01 +0000

    Addding updates

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/guildso/guild',
+  docsRepositoryBase: 'https://github.com/guildso/documentation',
+  titleSuffix: ' – Guild',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Guild</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Documentation
       </span>
     </>
   ),
