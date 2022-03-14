334c9b53b9bc078b578f5d343e9e22eb37502873
Tony Lea <tnylea@gmail.com>
2022-03-14 19:19:01 -0400

    Adding latest docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+
+import Prism from 'prism-react-renderer/prism'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+
+require("prismjs/components/prism-kotlin")
+require("prismjs/components/prism-csharp")
+
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
