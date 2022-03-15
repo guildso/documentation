e8a7c023112a0d52d7605b360b421bc4603d0c2c
Tony Lea <tnylea@gmail.com>
2022-03-15 12:36:59 -0400

    Adding updates to docs

--- theme.config.js
+++ theme.config.js
@@ -63,5 +63,5 @@ export default {
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
   footerText: <>MIT {new Date().getFullYear()} © Guild.</>,
-  unstable_faviconGlyph: '',
+  unstable_faviconGlyph: '📖',
 }
